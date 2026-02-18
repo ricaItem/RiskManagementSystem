06982502131c671e692325febc67c884dfdf4d9f
ricaItem <r.item.550993@umindanao.edu.ph>
2026-02-18 20:16:45 +0800

    added logout and profile settings in client

---
+++ Areas/Client/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_Sentro.Areas.Client.Controllers
+{
+    [Area("Client")]
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
